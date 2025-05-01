f38cfe433c71a259947933dfcc5e1ca0017424a6
Ian Torres <iantorres@outlook.com>
2025-05-01 14:09:50 -0400

    Fix.

--- benchmark.cpp
+++ benchmark.cpp
@@ -11,6 +11,7 @@
 #include <throttr/service.hpp>
 #include <throttr/protocol.hpp>
 #include <memory>
+#include <thread>
 
 using namespace boost::asio;
 using namespace throttr;
@@ -25,17 +26,25 @@ public:
         : io_context_(io_context), service_(service) {
     }
 
-    awaitable<void> consume_insert_only() const {
+    awaitable<void> consume_insert_only() {
         const std::string consumer_id = "consumer:insert-only";
         const std::string resource_id = "/api/insert-only";
 
-        constexpr int total = 1000;
+        constexpr int total = 1'000'000;
+        const auto ex = co_await this_coro::executor;
 
-        int remaining = total;
+        std::atomic e = total;
+        for (int i = 0; i < total; ++i) {
+            co_spawn(io_context_, [this, consumer_id, resource_id, total, &e]() -> awaitable<void> {
+                co_await service_.send<response_full>(
+                        request_insert_builder(total, 1, ttl_types::seconds, 5, consumer_id, resource_id));
+                --e;
+                co_return;
+            }, detached);
+        }
 
-        while (remaining > 0) {
-            co_await service_.send<response_full>(request_insert_builder(total, 1, ttl_types::seconds, 10, consumer_id, resource_id));
-            --remaining;
+        while (e.load(std::memory_order_acquire) > 0) {
+            co_await steady_timer(io_context_, chrono::nanoseconds(100)).async_wait(use_awaitable);
         }
 
         co_return;
@@ -45,7 +54,7 @@ public:
         const std::string consumer_id = "consumer:insert-update";
         const std::string resource_id = "/api/insert-update";
 
-        constexpr int total = 1000;
+        constexpr int total = 100'000;
 
         const auto insert_consume = request_insert_builder(total, 1, ttl_types::seconds, 10, consumer_id, resource_id);
         const auto response = co_await service_.send<response_full>(insert_consume);
@@ -65,8 +74,8 @@ public:
         co_return;
     }
 
-    awaitable<void> run_benchmark() const {
-        std::cout << std::endl << "Benchmark #1 - Quota: 1000, Usage: 1, TTL Type: Seconds - Using only Insert" <<
+    awaitable<void> run_benchmark() {
+        std::cout << std::endl << "Benchmark #1 - Quota: 1.000.000, Usage: 1, TTL Type: Seconds - Using only Insert" <<
                 std::endl;
         const auto start_insert_only = std::chrono::high_resolution_clock::now();
         co_await consume_insert_only();
@@ -79,7 +88,7 @@ public:
         std::cout << "Time: " << insert_only_elapsed_nanoseconds << " nanoseconds." << std::endl;
         std::cout << "Time: " << insert_only_elapsed_milliseconds << " milliseconds." << std::endl << std::endl;
 
-        std::cout << "Benchmark #2 - Quota: 1000, Usage: 1, TTL Type: Seconds - Using Insert and Updates" << std::endl;
+        std::cout << "Benchmark #2 - Quota: 10'000, Usage: 1, TTL Type: Seconds - Using Insert and Updates" << std::endl;
         const auto start_insert_and_update = std::chrono::high_resolution_clock::now();
         co_await consume_insert_and_update();
         const auto end_insert_and_update = std::chrono::high_resolution_clock::now();
@@ -100,7 +109,7 @@ private:
 
 int main() {
     io_context io_context;
-    const service_config config = {"throttr", 9000, 10};
+    const service_config config = {"127.0.0.1", 9000, 10};
     service _service(io_context.get_executor(), config);
 
     std::atomic _connected = false;
@@ -120,13 +129,21 @@ int main() {
         }
 
         std::puts("Service is connected. Starting benchmark...");
-        const Benchmark benchmark(io_context, _service);
+        Benchmark benchmark(io_context, _service);
         co_await benchmark.run_benchmark();
 
         co_return;
     }, detached);
 
+
+    std::vector<std::jthread> _threads_container;
+    _threads_container.reserve(3);
+    for (auto _i = 2; _i > 0; --_i)
+        _threads_container.emplace_back([&io_context] { io_context.run(); });
     io_context.run();
+
+    for (auto& _thead : _threads_container)
+        _thead.join();
 
     return 0;
 }

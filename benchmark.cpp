2452ba3e1b3fa4f5959b6fc8d3885b6d487d3261
Ian Torres <iantorres@outlook.com>
2025-05-19 21:59:57 -0400

    More realistic benchmark.

--- benchmark.cpp
+++ benchmark.cpp
@@ -50,7 +50,7 @@ int main() {
     std::this_thread::sleep_for(std::chrono::seconds(5));
 
     if (!error) {
-        constexpr int total = 100;
+        constexpr int total = 100'000;
         const std::string key = "resource|consumer";
         auto buffer = request_insert_builder(100, ttl_types::seconds, 10, key);
         for (int i = 0; i < total; ++i) {
